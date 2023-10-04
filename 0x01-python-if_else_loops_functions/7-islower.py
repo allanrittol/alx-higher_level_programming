3566ef206fb07fd7a767e79f7c42c34b9de35848
Allan Kamau <allanrittol@outlook.com>
2023-10-04 02:48:32 +0000

    a function that checks for lowercase character.

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    return 'a' <= c <= 'z'
+
+# Example usage:
+char_to_check = 'g'
+result = islower(char_to_check)
+print(f"Is '{char_to_check}' lowercase? {result}")
+
