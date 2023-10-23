f1f4171a271597b85c81b38da7363164f5ce1734
Allan Kamau <allanrittol@outlook.com>
2023-10-23 09:44:26 +0000

    a function that divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
+
     try:
         result = a / b
     except (TypeError, ZeroDivisionError):
-        print("Error: Division by zero is not allowed.")
+        result = None
     finally:
         print("Inside result: {}".format(result))
-        return (result)
+    return (result)
