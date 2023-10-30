8b1d65db764dac0271547b169105cc11aca98e11
Allan Kamau <allanrittol@outlook.com>
2023-10-30 18:04:41 +0000

    an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+""" Description"""
+
+
 class Rectangle:
+    """
+    This class represents a rectsngle. It is currently empty
+    and can be expanded by adding attributes and methods
+    """
     pass
