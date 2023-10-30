c69f361da6bf8e6dbd330a8051a174fcdfdc7883
Allan Kamau <allanrittol@outlook.com>
2023-10-30 18:37:05 +0000

    a function that executes a function safely

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!.usr/bin/python3
+
+""""Description"""
+
+
+class Rectangle:
+    """
+    Class represents a rectangle, and has a private instance attribute width
+    and height, with property setters and getters.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize the Rectangle with optional width and height
+        """
+        self.width = width
+        self.height - height
+
+        @property
+        def width(self):
+            """Retrieves the width of the Rectangle"""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """
+            sets the width of the Rectangle,
+            Raise TypeError if width is not an integer,
+            Raise Valueerror if width is less than 0
+            """
+            if not isinstance(value, int):
+                raise
+            TypeError("width must be an integer")
+            if value < 0:
+                raise
+            ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """Retrieve the height of the rectangle"""
+            return self.__height
