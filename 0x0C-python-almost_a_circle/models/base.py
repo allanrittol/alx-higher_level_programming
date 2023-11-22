457064ffc5c93b38febf664ba82507dc2e962623
allan kamau <allanrittol@outlook.com>
2023-11-22 08:58:05 +0000

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """The base class Module"""
-import json
-import csv
-import turtle
 
 
 class Base:
     """OOP heirachy representation of the base"""
 
-    __nb__objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Class Constructor"""
@@ -17,56 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-            @staticmethod
-    def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries.
-        Args:
-            list_dictionaries (list): A list of dictionaries.
-        """
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        to_json = json.dumps(list_dictionaries)
-
-        return to_json
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file.
-        Args:
-            list_objs (list): List of instances who inherits of Base
-        """
-        file_name = "{}.json".format(cls.__name__)
-
-        with open(file_name, "w") as jsonfile:
-            if list_objs is None:
-                jsonfile.write("[]")
-            else:
-                list_dict = []
-                for obj in list_objs:
-                    list_dict.append(obj.to_dictionary())
-                jsonfile.write(Base.to_json_string(list_dict))
-
-    def from_json_string(json_string):
-        """
-
-        """
-        if json_string is None or json_string == "[]":
-            return []
-        return json.loads(json_string)
-
-    @classmethod
-    def create(cls, **dictionary):
-        """Returns an instance with all attributes already set.
-        """
-        if dictionary and dictionary != {}:
-            if cls.__name__ == "Rectangle":
-                dummy = cls(1, 1)
-            else:
-                dummy = cls(1)
-            dummy.update(**dictionary)
-
-            return dummy
-
-    @classmethod
-    def load_from_file(cls):
