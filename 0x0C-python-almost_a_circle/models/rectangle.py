2deb71b86eadd83b8ab36d2e1961d514f08d250d
allan kamau <allanrittol@outlook.com>
2023-11-22 10:11:51 +0000

    the class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -44,3 +44,14 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         self.__y = value
+
+    if __name__ == "__main__":
+
+        r1 = Rectangle(10, 2)
+        print(r1.id)
+
+        r2 = Rectangle(2, 10)
+        print(r2.id)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        print(r3.id)
