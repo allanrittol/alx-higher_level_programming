6da7dbb2d4027de1c6d2e016a82ee2492013c45e
allan kamau <allanrittol@outlook.com>
2023-12-04 09:53:17 +0000

    a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new square
-        
+
         Args:
             size (int): The size of the new square
 
@@ -27,7 +27,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the current area of the square"""
         return (self.__size * self.__size)
