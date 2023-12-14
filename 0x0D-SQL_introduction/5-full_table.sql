aeaf4dbf0897a45ec9977057623ab86d595683fe
Allan Kamau <allanrittol@outlook.com>
2023-12-14 17:49:03 +0000

    script that displays the number of records

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,1 @@
--- A script that prints the fulldescripiton of the first_table anemUSE hbtn_0c_0;
-DESCRIBE first_table;
+-- A script that prints the fulldescripiton of the first_table anemSELECT COLUMN_NAME, DATA_TYPE,;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,5 @@
 -- A script that displays the number of records
-
+SELECT COUNT(*) AS
+NumberOfRecords
+FROM first_table
+WHERE id = 89;
