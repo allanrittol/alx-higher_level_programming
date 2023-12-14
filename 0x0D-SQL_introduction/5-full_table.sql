c9408ef8c7503e33fbd9dbcc9a545a49e458156a
Allan Kamau <allanrittol@outlook.com>
2023-12-14 19:20:03 +0000

    script that prints the full description of the able

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,1 +1,5 @@
--- A script that prints the fulldescripiton of the first_table anemSELECT COLUMN_NAME, DATA_TYPE,;
+-- A script that prints the fulldescripiton of the first_table
+SELECT COLUMN_NAME, DATA_TYPE,
+IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
