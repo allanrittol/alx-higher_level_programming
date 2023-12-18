41929a7467d11e4a75b5627455f2df5b95c5c59e
Allan Kamau <allanrittol@outlook.com>
2023-12-18 09:06:45 +0000

    a script that lists all the cities in the database

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- A script that lists all the cities of California in a database
+SELECT cities.id, cities.name
+FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
