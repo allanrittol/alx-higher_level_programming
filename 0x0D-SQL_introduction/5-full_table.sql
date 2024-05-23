eb9bdbb7dc786037933af28d03262067cfd0ff4a
allan kamau <allanrittol@outlook.com>
2024-05-23 09:51:10 +0000

    Script that prints the full description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- A script that prints the fulldescripiton of the first_table
-SELECT COLUMN_NAME, DATA_TYPE,
-IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+SHOW CREATE TABLE 'first_table';
